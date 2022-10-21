4b0110921a44d9e2b7c6486445c555070a68b1b7
Thomas Depian <thomas.depian99@gmail.com>
2022-10-22 00:48:51 +0200

    some fixes and improvements

--- script.js
+++ script.js
@@ -5,6 +5,16 @@ function strToBool(s) {
     return regex.test(s);
 }
 
+function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.webkitRequestFullscreen) { /* Safari */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE11 */
+      elem.msRequestFullscreen();
+    }
+  }
+
 function isNumeric(str) {
     if (typeof str != "string") return false // we only process strings!  
     return !isNaN(str) && // use type coercion to parse the _entirety_ of the string (`parseFloat` alone does not do this)...
@@ -25,9 +35,9 @@ function fillCaseInfo() {
         document.getElementById("button-next-case").style.display = "none";
     } else {
         if (isNumeric(caseNr)) {
-            document.getElementById("link-next-case").setAttribute("href", `/black-or-white/cases/case-${(+caseNr) + 1}.html`);
+            document.getElementById("link-next-case").setAttribute("href", `/cases/case-${(+caseNr) + 1}.html`);
         } else {
-            document.getElementById("link-next-case").setAttribute("href", '/black-or-white/index.html');
+            document.getElementById("link-next-case").setAttribute("href", '/index.html');
         }
         
     }
